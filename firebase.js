b9494c62907765f6c13728f59f48f8ff70016ddf
Arpit Srivastav <arpitsriv28@gmail.com>
2024-08-06 01:28:03 +0300

    Update firebase.js configuration and update .gitignore

--- firebase.js
+++ firebase.js
@@ -1,20 +1,25 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+console.log("API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log("Auth Domain:", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
+console.log("Project ID:", process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
+
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDW7rLSVMSkVHbnJt9a3xPskaz05bHS7ts",
-  authDomain: "pantry-management-b0f32.firebaseapp.com",
-  projectId: "pantry-management-b0f32",
-  storageBucket: "pantry-management-b0f32.appspot.com",
-  messagingSenderId: "954605942748",
-  appId: "1:954605942748:web:2260bb56c080ac53bd4119",
-  measurementId: "G-Z1P5TB0BKD"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
+console.log(firebaseConfig)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
